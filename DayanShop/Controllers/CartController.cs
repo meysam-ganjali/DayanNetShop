a06879f059b764848e24f8e946b154a24296103d
unknown <meysam.ghanjali1365@hotmail.com>
2023-01-03 18:58:39 +0330

    Fix Product Lst In Shoping Controller

---
+++ DayanShop/Controllers/CartController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DayanShop.Controllers
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+        public IActionResult CartList(int id)
+        {
+            return View();
+        }
+    }
+}
--- DayanShop/Controllers/ShopController.cs
+++ DayanShop/Controllers/ShopController.cs
@@ -1,4 +1,5 @@
-﻿using DayanShop.Application.FacadePattern.FSDShoping;
+﻿using DayanShop.Application.FacadePattern.FSDProduct;
+using DayanShop.Application.FacadePattern.FSDShoping;
 using DayanShop.Application.StoreServices.Queries.Shoping;
 using DayanShop.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -8,14 +9,22 @@ namespace DayanShop.Controllers
     public class ShopController : Controller
     {
         private readonly IFSDShoping _shopService;
+        private readonly IFSDProduct _product;
 
-        public ShopController(IFSDShoping shopService)
+        public ShopController(IFSDShoping shopService, IFSDProduct product)
         {
             _shopService = shopService;
+            _product = product;
         }
-        public IActionResult Index(Ordering ordering, string Searchkey, int? CatId = null, int page = 1, int pageSize = 2)
+        public IActionResult Index(Ordering ordering, string Searchkey, int? CatId = null, int page = 1, int pageSize = 50)
         {
             var result = _shopService.FetchProductWithFilter.Execute(ordering, Searchkey, page, pageSize, CatId);
+            return View(result.Data);
+        }
+
+        public async Task<IActionResult> ProductDetaile(int id)
+        {
+            var result = await _product.GetProductDetails.ProductDetailesAsync(id);
             return View(result.Data);
         }
     }
