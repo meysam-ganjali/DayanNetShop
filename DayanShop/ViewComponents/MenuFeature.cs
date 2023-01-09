871fcab91e902968d16fd404b9e63c53b50d2e85
unknown <meysam.ghanjali1365@hotmail.com>
2023-01-09 16:13:43 +0330

    Add Menu

---
+++ DayanShop/ViewComponents/MenuFeature.cs
@@ -0,0 +1,20 @@
+﻿using DayanShop.Application.FacadePattern.FSDCategory;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DayanShop.ViewComponents;
+
+public class MenuFeature: ViewComponent
+{
+    private readonly IFSDPatternCategory _cat;
+
+    public MenuFeature(IFSDPatternCategory cat)
+    {
+        _cat = cat;
+    }
+     public async Task<IViewComponentResult> InvokeAsync()
+     {
+         var menuItem = await _cat.CategoryInformation.GetAllParentCategoryAsync(null);
+            return View(viewName: "GetMenu", menuItem.Data);
+        }
+
+}
